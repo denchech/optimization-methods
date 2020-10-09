fc005d542e471ebdda4ecfd50f77e63acf61f60e
denchech <d_cher00@mail.ru>
2020-10-09 20:57:14 +0300

    update golden ratio algorithm

--- Lab1/algorithms/golden_ratio_algorithm.py
+++ Lab1/algorithms/golden_ratio_algorithm.py
@@ -1,23 +1,20 @@
-from algorithms.abstract_algorithm import  AbstractAlgorithm
+from algorithms.abstract_algorithm import AbstractAlgorithm
 
 
 class GoldenRatioAlgorithm(AbstractAlgorithm):
     __K = (1 + 5 ** 0.5) / 2
 
-    def __init__(self, func, a: float, b: float, eps: float, *params) -> None:
-        super().__init__(func, a, b, eps)
+    def calculate(self, func, a, b, eps) -> float:
+        while abs(b - a) >= eps:
+            x1 = b - (b - a) / self.__K
+            x2 = a + (b - a) / self.__K
 
-    def calculate(self) -> float:
-        while abs(self.b - self.a) >= self.eps:
-            x1 = self.b - (self.b - self.a) / self.__K
-            x2 = self.a + (self.b - self.a) / self.__K
-
-            y1 = self.func(x1)
-            y2 = self.func(x2)
+            y1 = func(x1)
+            y2 = func(x2)
 
             if y1 >= y2:
-                self.a = x1
+                a = x1
             else:
-                self.b = x2
+                b = x2
 
-        return (self.b + self.a) / 2
+        return (b + a) / 2
--- Lab1/tests/test_golden_ratio_algorithm.py
+++ Lab1/tests/test_golden_ratio_algorithm.py
@@ -1,62 +1,13 @@
-from unittest import TestCase
 from algorithms.golden_ratio_algorithm import GoldenRatioAlgorithm
-from tests import test_functions as functions
+from tests.test_base import TestBase
+from algorithms.abstract_algorithm import AbstractAlgorithm
 
 
-class TestGoldenRatioAlgorithm(TestCase):
-    __PLACES = 4
+class TestGoldenRatioAlgorithm(TestBase):
 
-    def test_calculate_first(self):
-        func = functions.first
-        a = functions.first_interval()['a']
-        b = functions.first_interval()['b']
-        eps = functions.accuracy()
-        algorithm = GoldenRatioAlgorithm(func, a, b, eps)
+    def algorithm(self) -> AbstractAlgorithm:
+        return GoldenRatioAlgorithm()
 
-        result = algorithm.calculate()
-
-        self.assertAlmostEqual(result, functions.first_answer(), places=self.__PLACES)
-
-    def test_calculate_second(self):
-        func = functions.second
-        a = functions.second_interval()['a']
-        b = functions.second_interval()['b']
-        eps = functions.accuracy()
-        algorithm = GoldenRatioAlgorithm(func, a, b, eps)
-
-        result = algorithm.calculate()
-
-        self.assertAlmostEqual(result, functions.second_answer(), places=self.__PLACES)
-
-    def test_calculate_third(self):
-        func = functions.third
-        a = functions.third_interval()['a']
-        b = functions.third_interval()['b']
-        eps = functions.accuracy()
-        algorithm = GoldenRatioAlgorithm(func, a, b, eps)
-
-        result = algorithm.calculate()
-
-        self.assertAlmostEqual(result, functions.third_answer(), places=self.__PLACES)
-
-    def test_calculate_fourth(self):
-        func = functions.fourth
-        a = functions.fourth_interval()['a']
-        b = functions.fourth_interval()['b']
-        eps = functions.accuracy()
-        algorithm = GoldenRatioAlgorithm(func, a, b, eps)
-
-        result = algorithm.calculate()
-
-        self.assertAlmostEqual(result, functions.fourth_answer(), places=self.__PLACES)
-
-    def test_calculate_fifth(self):
-        func = functions.fifth
-        a = functions.fifth_interval()['a']
-        b = functions.fifth_interval()['b']
-        eps = functions.accuracy()
-        algorithm = GoldenRatioAlgorithm(func, a, b, eps)
-
-        result = algorithm.calculate()
-
-        self.assertAlmostEqual(result, functions.fifth_answer(), places=self.__PLACES)
+    def test_algorithm(self):
+        for i in range(5):
+            self.calculate(i)
