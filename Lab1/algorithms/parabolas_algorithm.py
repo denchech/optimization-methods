e429424095ff344a6d78dae7d25713e9b6ebf0af
denchech <d_cher00@mail.ru>
2020-10-09 22:46:21 +0300

    update working version

--- Lab1/algorithms/parabolas_algorithm.py
+++ Lab1/algorithms/parabolas_algorithm.py
@@ -6,23 +6,33 @@ class ParabolasAlgorithm(AbstractAlgorithm):
         x1 = a
         x3 = b
         x2 = (a + b) / 2
+        f1 = func(x1)
+        f2 = func(x2)
+        f3 = func(x3)
 
         while abs(x3 - x1) >= eps:
-            f1 = func(x1)
-            f2 = func(x2)
-            f3 = func(x3)
             u = self.__get_minimum(x1, f1, x2, f2, x3, f3)
             fu = func(u)
             if u <= x2 and fu > f2:
                 x1 = u
+                f1 = fu
+                continue
             elif u > x2 and fu > f2:
                 x3 = u
+                f3 = fu
+                continue
             elif u > x2 and fu <= f2:
                 x1 = x2
+                f1 = f2
                 x2 = u
+                f2 = fu
+                continue
             elif u <= x2 and fu <= f2:
                 x3 = x2
+                f3 = f2
                 x2 = u
+                f2 = fu
+                continue
 
         return (x1 + x3) / 2
 
