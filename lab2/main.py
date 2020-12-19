51778680a954d2dcfe47483f0ae081b109dd5bac
denchech <d_cher00@mail.ru>
2020-12-19 12:06:23 +0300

    implement brent algorithm with derivative + report

--- lab2/main.py
+++ lab2/main.py
@@ -1,13 +1,14 @@
 import sympy as sp
 import numpy as np
-from prettytable import PrettyTable
 from matplotlib import pyplot as plt
 from lab1.algorithms.dichotomy_algorithm import DichotomyAlgorithm
+from lab2.algorithms.brent_diff import BrentDiff
 from lab2.algorithms.fastest_descent_algorithm import FastestDescent
 from lab2.algorithms.projection_descent_algorithm import ProjectionDescent
 from lab2.algorithms.ravine_descent_algorithm import RavineDescent
 from lab2.algorithms.coordinate_descent import CoordinateDescent
 from lab2.projections.projection import LineProjection
+from prettytable import PrettyTable
 
 
 def f1(x1, x2):
@@ -24,6 +25,26 @@ def f3(x1, x2):
 
 def f4(x1, x2, x3, x4):
     return (x1 + x2) ** 2 + 5 * (x3 - x4) ** 2 + (x2 - 2 * x3) ** 4 + 10 * (x1 - x4) ** 4
+
+
+def f_brent_1(x):
+    return -5 * x ** 5 + 4 * x ** 4 - 12 * x ** 3 + 11 * x ** 2 - 2 * x + 1
+
+
+def f_brent_2(x):
+    return sp.log(x - 2, 10) ** 2 + sp.log(10 - x, 10) ** 2 - x ** 0.2
+
+
+def f_brent_3(x):
+    return -3 * x * sp.sin(0.75 * x) + sp.exp(-2 * x)
+
+
+def f_brent_4(x):
+    return sp.exp(3 * x) + 5 * sp.exp(-2 * x)
+
+
+def f_brent_5(x):
+    return 0.2 * x * sp.log(x, 10) + (x - 2.3) ** 2
 
 
 functions_for_plots = [
@@ -161,11 +182,48 @@ def analyze_cd():
     print(get_table([r1, r2, r3, r4], [ff1, ff2, ff3, ff4]).get_string())
 
 
+def analyze_brent():
+    x = sp.symbols('x')
+    alg = BrentDiff(x)
+
+    eps_pows = list(range(-10, 0))
+    y = []
+    plt.title(f'\'Brent with derivative\', f1')
+    plt.ylabel('Iterations')
+    plt.xlabel('log10(eps)')
+    for eps_pow in eps_pows:
+        print(eps_pow)
+        y.append(
+            alg.calculate(f_brent_1(x), -0.5, 0.5, 10 ** eps_pow).iterations
+        )
+    plt.grid()
+    plt.plot(eps_pows, y)
+    plt.show()
+
+    ff1 = f_brent_1(x)
+    r1 = alg.calculate(ff1, -0.5, 0.5, 1e-5)
+
+    ff2 = f_brent_2(x)
+    r2 = alg.calculate(ff2, 6.0, 9.9, 1e-5)
+
+    ff3 = f_brent_3(x)
+    r3 = alg.calculate(ff3, 0.0, 2 * np.pi, 1e-5)
+
+    ff4 = f_brent_4(x)
+    r4 = alg.calculate(ff4, 0.0, 1.0, 1e-5)
+
+    ff5 = f_brent_5(x)
+    r5 = alg.calculate(ff5, 0.5, 2.5, 1e-5)
+
+    print(get_table([r1, r2, r3, r4, r5], [ff1, ff2, ff3, ff4, ff5]).get_string())
+
+
 def main():
     analyze_fgd()
     analyze_pgd()
     analyze_rgd()
     analyze_cd()
+    analyze_brent()
 
 
 if __name__ == '__main__':
